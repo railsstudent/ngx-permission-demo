f0d9c0a04230dc81f8a6fd822830d6a187628acb
railsstudent <cleung334@gmail.com>
2020-01-17 12:07:43 +0800

    Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgxPermissionsModule } from 'ngx-permissions';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouteAComponent } from './route-a/route-a.component';
+import { RouteBComponent } from './route-b/route-b.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule, 
-    NgxPermissionsModule.forRoot() 
+    NgxPermissionsModule.forRoot(),
+    AppRoutingModule 
   ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, RouteAComponent, RouteBComponent ],
   bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
---
+++ src/app/route-a/route-a.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-route-a',
+  templateUrl: './route-a.component.html',
+  styleUrls: ['./route-a.component.scss']
+})
+export class RouteAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
