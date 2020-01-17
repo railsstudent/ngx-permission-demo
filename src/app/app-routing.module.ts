9f869b9c7536b4dee4cc25a3fe5377392cb95a9c
railsstudent <cleung334@gmail.com>
2020-01-17 12:12:19 +0800

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RouteAComponent } from './route-a/router-a.component';
+import { RouteBComponent } from './route-b/router-b.component';
 
 const routes: Routes = [
-    
+  {
+    path: 'route-a',
+    component: RouteAComponent
+  },
+    {
+    path: 'route-b',
+    component: RouteBComponent
+  }
 ];
 
 @NgModule({
